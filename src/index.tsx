f35cd01735faadd9e57f9dbc9ed4dc0979e0cbe8
Brandon Mellus <mellusbrandon@gmail.com>
2019-07-26 19:59:31 -0400

    Edit so code will run properly

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/App";
 
-import { store } from "./components/redux";
-
 ReactDOM.render(
   (
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <App />
   ),
   document.getElementById("root"),
 );
