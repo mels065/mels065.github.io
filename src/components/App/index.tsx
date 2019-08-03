107d61fd89b363ff83a2c96990cdd8b92b6d8e04
Brandon Mellus <mellusbrandon@gmail.com>
2019-08-03 18:25:57 -0400

    Add Font Awesome icons

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,20 +1,9 @@
 import * as React from "react";
 
-import Image from "../atoms/Image";
-
 import "./style.scss";
-
-function test(mode: string) {
-  console.log(mode);
-}
 
 export default function App(): JSX.Element {
   return (
-    <div id="App">
-      <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg",
-        alt="A cat"
-      />
-    </div>
+    <div id="App" />
   );
 }
